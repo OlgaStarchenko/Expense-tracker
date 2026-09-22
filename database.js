00ef6fe7062c875b7da19d6e159b1df2ce0507f3
OlgaStarchenko <o9915530@gmail.com>
2026-09-22 10:02:42 +1000

    Проект инициализирован. Создан .gitignore с исключением node_modules. Установлены express и sql.js. Создан database.js для создания, загрузки и сохранения SQLite-базы данных.

---
+++ database.js
@@ -0,0 +1,43 @@
+import initSqlJs from "sql.js";
+import fs from "fs";
+import path from "path";
+
+const SQL = await initSqlJs();
+//загружает sqlite
+const dataDirectory = path.resolve("data");
+//ищем папку data, внутри которой база данных
+
+const databasePath = path.join(dataDirectory, "database.sqlite");
+//ищем сам файл базы данных
+
+if (!fs.existsSync(dataDirectory)) {
+  fs.mkdirSync(dataDirectory);
+}
+//если нет папки data, то мы создаем папку data
+
+let db;
+// создаем переменную для базы данных
+
+if (fs.existsSync(databasePath)) {
+  const fileBuffer = fs.readFileSync(databasePath);
+  //получаем и читаем файл базы данных
+
+  db = new SQL.Database(fileBuffer);
+  //загружаем существующую базу данных
+} else {
+  db = new SQL.Database();
+  //иначе создаем пустую базу данных
+}
+
+function saveDatabase() {
+  const data = db.export();
+  //экспортируем базу данных
+  const buffer = Buffer.from(data);
+  //получаем ее обновленное содержимое
+  fs.writeFileSync(databasePath, buffer);
+  //записываем обновление в существующий файл бд
+}
+//функция для сохранения изменений в базу данных
+
+export { db, saveDatabase };
+//Экспортируем переменную базы данных и функцию обновление в существующий файл бд
